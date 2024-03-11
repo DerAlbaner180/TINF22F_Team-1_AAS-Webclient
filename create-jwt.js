c84354842f3da3a73ae1bf6a847895c3fbbd48a7
Ilir E. <72442005+DerAlbaner180@users.noreply.github.com>
2024-03-11 23:31:58 +0100

    Deleted old template for project

--- create-jwt.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const token = jwt.sign({
-    id: 'john.doe@email.com',
-    name: 'John Doe',
-    role: 'editor'
-}, 'The quick brown fox jumps over the lazy dog.');
-
-console.log(token);
